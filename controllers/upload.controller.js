6679bbf1c56630464a92ad4287b3900caae45811
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-09-03 16:14:53 +0200

    Adding download

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -28,10 +28,14 @@ exports.all = async (req, res, next) => {
   console.log('live', live);
   console.log('test', test);
   test.forEach(async file => {
-    console.log('@Download Starting');
-    const result = await download(
-      path.join(process.env.TEST_DOWNLOAD_API, file)
-    ).pipe(createWriteStream(path.join(process.env.TEST_LOGS_PATH, file)));
-    console.log('@Download Done', result);
+    try {
+      console.log('@Download Starting');
+      await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
+        createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
+      );
+      console.log('@Download Done', result);
+    } catch (error) {
+      console.log('@Download Error', error);
+    }
   });
 };
