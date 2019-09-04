6524b11b1699fff9641aaab99915d2d2cebf3fa4
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-09-04 11:38:53 +0200

    Fixing issue

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -28,20 +28,23 @@ exports.all = async (req, res, next) => {
     console.log('old', old);
     console.log('live', live);
     console.log('test', test);
-    test.forEach(async file => {
-      try {
-        console.log('@Download Starting');
-        console.log(path.join(process.env.TEST_DOWNLOAD_API, file));
-        console.log(path.join(process.env.TEST_LOGS_PATH, file));
-        await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
-          createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
-        );
-        console.log('@Download Done');
-      } catch (error) {
-        console.log('@Download Error', error);
-        return;
-      }
-    });
+    await download('http://ipv4.download.thinkbroadband.com/5MB.zip').pipe(
+      createWriteStream('/home/lyticshub/Field_Logs/Test/down.zip')
+    );
+    // test.forEach(async file => {
+    //   try {
+    //     console.log('@Download Starting');
+    //     console.log(path.join(process.env.TEST_DOWNLOAD_API, file));
+    //     console.log(path.join(process.env.TEST_LOGS_PATH, file));
+    //     await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
+    //       createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
+    //     );
+    //     console.log('@Download Done');
+    //   } catch (error) {
+    //     console.log('@Download Error', error);
+    //     return;
+    //   }
+    // });
     return res.status(200).send({ success: true, message: 'Success' });
   } catch (err) {
     console.log('@Error', { error });
