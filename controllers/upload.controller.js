a0fa75cd78729bb9a3a1e73e8ef8faa6e9b83efb
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-09-04 10:32:10 +0200

    Fixing issue

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -16,7 +16,7 @@ exports.uploadToTest = (req, res, next) => {
   upload(req, res, next, process.env.TEST_LOGS_PATH);
 };
 
-exports.all = (req, res, next) => {
+exports.all = async (req, res, next) => {
   if (req.header('Upload-Auth') !== process.env.SECRET_KEY.toString()) {
     console.log('Upload-Auth not correct');
     return res
@@ -27,12 +27,12 @@ exports.all = (req, res, next) => {
   console.log('old', old);
   console.log('live', live);
   console.log('test', test);
-  test.forEach(file => {
+  test.forEach(async file => {
     try {
       console.log('@Download Starting');
       console.log(path.join(process.env.TEST_DOWNLOAD_API, file));
       console.log(path.join(process.env.TEST_LOGS_PATH, file));
-      download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
+      await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
         createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
       );
       console.log('@Download Done');
@@ -41,4 +41,5 @@ exports.all = (req, res, next) => {
       return;
     }
   });
+  return res.status(200).send({ success: true, message: 'Success' });
 };
