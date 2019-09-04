0b50f7695a6ca30bd4dbc65fb464821ecfa4f2f4
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-09-04 10:39:36 +0200

    Fixing issue

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -16,7 +16,7 @@ exports.uploadToTest = (req, res, next) => {
   upload(req, res, next, process.env.TEST_LOGS_PATH);
 };
 
-exports.all = (req, res, next) => {
+exports.all = async (req, res, next) => {
   try {
     if (req.header('Upload-Auth') !== process.env.SECRET_KEY.toString()) {
       console.log('Upload-Auth not correct');
@@ -28,20 +28,20 @@ exports.all = (req, res, next) => {
     console.log('old', old);
     console.log('live', live);
     console.log('test', test);
-    // test.forEach(async file => {
-    //   try {
-    //     console.log('@Download Starting');
-    //     console.log(path.join(process.env.TEST_DOWNLOAD_API, file));
-    //     console.log(path.join(process.env.TEST_LOGS_PATH, file));
-    //     await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
-    //       createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
-    //     );
-    //     console.log('@Download Done');
-    //   } catch (error) {
-    //     console.log('@Download Error', error);
-    //     return;
-    //   }
-    // });
+    test.forEach(async file => {
+      try {
+        console.log('@Download Starting');
+        console.log(path.join(process.env.TEST_DOWNLOAD_API, file));
+        console.log(path.join(process.env.TEST_LOGS_PATH, file));
+        await download(path.join(process.env.TEST_DOWNLOAD_API, file)).pipe(
+          createWriteStream(path.join(process.env.TEST_LOGS_PATH, file))
+        );
+        console.log('@Download Done');
+      } catch (error) {
+        console.log('@Download Error', error);
+        return;
+      }
+    });
     return res.status(200).send({ success: true, message: 'Success' });
   } catch (err) {
     console.log('@Error', { error });
